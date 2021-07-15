6c73bfb184d6a6ddc7f8570cd72cb61151a852fb
Ever2013 <ever2013@mail.ru>
2021-07-15 10:55:51 +0300

    edit translate

--- upload/admin/language/en-gb/extension/module/oasiscatalog.php
+++ upload/admin/language/en-gb/extension/module/oasiscatalog.php
@@ -11,7 +11,20 @@ $_['text_extension'] = 'Extensions';
 $_['text_success'] = 'Success: You have modified!';
 $_['text_edit'] = 'Edit';
 $_['text_complete'] = 'Imported';
-$_['text_loading'] = 'Загружаем';
+$_['text_loading'] = 'loading';
+$_['text_currency'] = 'Currency';
+$_['text_no_vat'] = 'No VAT';
+$_['text_not_on_order'] = 'Without goods "on order"';
+$_['text_price'] = 'Price:';
+$_['text_price_from'] = 'Price from';
+$_['text_price_to'] = 'Price to';
+$_['text_select_categories'] = 'Select Categories';
+$_['text_only_new'] = 'Only new';
+$_['text_only_hits'] = 'Only hits';
+$_['text_only_discount'] = 'Only with a discount';
+$_['text_warehouse_moscow'] = 'Warehouse in Moscow';
+$_['text_warehouse_europe'] = 'Warehouse in Europe';
+$_['text_remote_warehouse'] = 'Remote warehouse';
 
 // Entry
 $_['entry_status'] = 'Status';
--- upload/admin/language/ru-ru/extension/module/oasiscatalog.php
+++ upload/admin/language/ru-ru/extension/module/oasiscatalog.php
@@ -12,6 +12,19 @@ $_['text_success'] = 'Настройки модуля обновлены!';
 $_['text_edit'] = 'Редактирование';
 $_['text_complete'] = 'Загружено';
 $_['text_loading'] = 'Загружаем';
+$_['text_currency'] = 'Валюта';
+$_['text_no_vat'] = 'Без НДС';
+$_['text_not_on_order'] = 'Без товаров "под заказ"';
+$_['text_price'] = 'Цена:';
+$_['text_price_from'] = 'от';
+$_['text_price_to'] = 'до';
+$_['text_select_categories'] = 'Выбрать рубрики';
+$_['text_only_new'] = 'Только новинки';
+$_['text_only_hits'] = 'Только хиты';
+$_['text_only_discount'] = 'Только со скидкой';
+$_['text_warehouse_moscow'] = 'На складе в Москве';
+$_['text_warehouse_europe'] = 'На складе в Европе';
+$_['text_remote_warehouse'] = 'На удаленном складе';
 
 // Entry
 $_['entry_status'] = 'Статус';
